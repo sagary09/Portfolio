4fbb3d6485a55150fab45e450b072b2b829e2194
sagary09 <sagaryukta@gmail.com>
2016-12-28 17:20:58 -0500

    hover image

--- js/freelancer.js
+++ js/freelancer.js
@@ -1,5 +1,59 @@
 // Freelancer Theme JavaScript
 
+//EMS-Speech//
+
+function rollover1(ems_speech_img){
+  ems_speech_img.src = 'img/ems-speech-color.png';
+}
+function mouseaway1(ems_speech_img)
+{
+  ems_speech_img.src = 'img/ems-speech-BW.png';
+}
+//DreamOn//
+
+function rollover2(dream_img){
+  dream_img.src = 'img/dream-color.png';
+}
+function mouseaway2(dream_img)
+{
+  dream_img.src = 'img/dream-BW.png';
+}
+//Craigslist//
+
+function rollover3(craig_img){
+  craig_img.src = 'img/craigslist-color.png';
+}
+function mouseaway3(craig_img)
+{
+  craig_img.src = 'img/craigslist-BW.png';
+}
+//Transfer//
+
+function rollover4(transfer_img){
+  transfer_img.src = 'img/transfer-color.png';
+}
+function mouseaway4(transfer_img)
+{
+  transfer_img.src = 'img/Transfer-BW.png';
+}
+//Federal//
+
+function rollover5(federal_img){
+  federal_img.src = 'img/federal-color.png';
+}
+function mouseaway5(federal_img)
+{
+  federal_img.src = 'img/federal-BW.png';
+}
+//Global//
+
+function rollover6(global_img){
+  global_img.src = 'img/global-color.png';
+}
+function mouseaway6(global_img)
+{
+  global_img.src = 'img/global-BW.png';
+}
 (function($) {
     "use strict"; // Start of use strict
 
@@ -19,7 +73,7 @@
     });
 
     // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function(){ 
+    $('.navbar-collapse ul li a').click(function(){
             $('.navbar-toggle:visible').click();
     });
 
@@ -40,5 +94,6 @@
             $(this).removeClass("floating-label-form-group-with-focus");
         });
     });
+
 
 })(jQuery); // End of use strict
